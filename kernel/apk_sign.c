f136a51f0f703982c28bccfb8077894494050e6c
Frost <frost.github@proton.me>
2025-08-31 18:48:15 +0530

    Revert "kernel: nest ksun switch manager support under CONFIG_KSU_SWITCH_MANAGER config default as disabled"
    This reverts commit e0c461322bca8ab698e3e9749a7d467f67f5daa4.

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -314,9 +314,7 @@ static struct kernel_param_ops expected_size_ops = {
 module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 		&ksu_debug_manager_uid, S_IRUSR | S_IWUSR);
 
-#endif
-
-#ifdef CONFIG_KSU_SWITCH_MANAGER
+#else
 
 static int set_expected_size(const char *val, const struct kernel_param *kp)
 {
@@ -387,5 +385,9 @@ bool is_manager_apk(char *path)
 	pr_info("%s: expected size: %u, expected hash: %s\n",
 		path, expected_manager_size, expected_manager_hash);
 
+#ifdef CONFIG_KSU_DEBUG
+	return check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH);
+#else
 	return check_v2_signature(path, expected_manager_size, expected_manager_hash);
+#endif
 }
